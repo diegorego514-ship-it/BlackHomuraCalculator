b30826cf265084f9df4bfa840da537254d4da0bd
diegorego514-ship-it <diegorego514@gmail.com>
2025-12-26 13:03:19 -0300

    Add files via upload

---
+++ blackhomuracalculator.py
@@ -0,0 +1,17 @@
+import os
+import sys
+
+def int(b):
+
+    def int (a = b + 5 * 10):
+    
+        def int (result):
+            result, int(result)
+            result (a = b + 5 * 10)
+            int (result)
+            print(f"[''write in your age: '']")
+            print(f"[''write in your name: '']")
+            try: 
+                print(f"[''write in what you want to input in:'']")
+            except:
+                print(f"['You have your first print'']")
