a795cc56d9374fc64a57d468068878944152abb3
diegorego514-ship-it <diegorego514@gmail.com>
2026-01-07 19:09:31 -0300

    Update blackhomuracalculator.py

--- blackhomuracalculator.py
+++ blackhomuracalculator.py
@@ -4,7 +4,7 @@ terminal screen."""
     x = int(input("Num1:"))
     y = int(input("Num2:"))
     z = int(input("Num3:"))
-    num = 5
+    num = "5"
     if num == x or y or z:
         print(f"[*] Type in the number that the user has to input.")
 
@@ -17,11 +17,11 @@ terminal screen."""
     y = int(input("Age2:"))
     z = int(input("Age3:"))
     
-    age = 18
+    age = "18"
     if age == x or y or z:
         print(f"[*] Type in the age that the user has to input.")
 
-    if age >= 18:
+    if age == x or y or z:
         print(f"You are {age} year old and a adult")
     else:
         print(f"You are {age} year old. Get a life and study hard.")
